9590d17682c86a8d5c361f1a6e009f5056abe261
CHZEE93 <zndn887@gmail.com>
2025-01-15 22:07:56 +0900

    특정 게시물 불러오기 API 구현

--- board_back/src/main/java/com/jiraynor/board_back/controller/BoardController.java
+++ board_back/src/main/java/com/jiraynor/board_back/controller/BoardController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jiraynor.board_back.dto.request.board.PostBoardRequestDto;
+import com.jiraynor.board_back.dto.response.board.GetBoardResponseDto;
 import com.jiraynor.board_back.dto.response.board.PostBoardResponseDto;
 import com.jiraynor.board_back.service.BoardService;
 
@@ -14,12 +15,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/v1/board")
 @RequiredArgsConstructor
 public class BoardController {
     private final BoardService boardService;
+
+    @GetMapping("/{boardNumber}")
+    public ResponseEntity<? super GetBoardResponseDto> getBoard(
+            @PathVariable("boardNumber") Integer boardNumber) {
+        ResponseEntity<? super GetBoardResponseDto> response = boardService.getBoard(boardNumber);
+        return response;
+    }
 
     @PostMapping("")
     public ResponseEntity<? super PostBoardResponseDto> postBoard(
--- board_back/src/main/java/com/jiraynor/board_back/service/implement/BoardServiceImplement.java
+++ board_back/src/main/java/com/jiraynor/board_back/service/implement/BoardServiceImplement.java
@@ -8,12 +8,14 @@ import org.springframework.stereotype.Service;
 
 import com.jiraynor.board_back.dto.request.board.PostBoardRequestDto;
 import com.jiraynor.board_back.dto.response.ResponseDto;
+import com.jiraynor.board_back.dto.response.board.GetBoardResponseDto;
 import com.jiraynor.board_back.dto.response.board.PostBoardResponseDto;
 import com.jiraynor.board_back.entity.BoardEntity;
 import com.jiraynor.board_back.entity.ImageEntity;
 import com.jiraynor.board_back.repository.BoardRepository;
 import com.jiraynor.board_back.repository.ImageRepository;
 import com.jiraynor.board_back.repository.UserRepository;
+import com.jiraynor.board_back.repository.resultSet.GetBoardResultSet;
 import com.jiraynor.board_back.service.BoardService;
 
 import lombok.RequiredArgsConstructor;
@@ -25,6 +27,31 @@ public class BoardServiceImplement implements BoardService {
     private final UserRepository userRepository;
     private final BoardRepository boardRepository;
     private final ImageRepository imageRepository;
+
+    @Override
+    public ResponseEntity<? super GetBoardResponseDto> getBoard(Integer boardNumber) {
+
+        GetBoardResultSet resultSet = null;
+        List<ImageEntity> imageEntities = new ArrayList<>();
+
+        try {
+            resultSet = boardRepository.getBoard(boardNumber);
+            if (resultSet == null)
+                return GetBoardResponseDto.noExistBoard();
+
+            imageEntities = imageRepository.findByBoardNumber(boardNumber);
+
+            BoardEntity boardEntity = boardRepository.findByBoardNumber(boardNumber);
+            boardEntity.increaseViewCount();
+            boardRepository.save(boardEntity);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+
+        return GetBoardResponseDto.success(resultSet, imageEntities);
+    }
 
     @Override
     public ResponseEntity<? super PostBoardResponseDto> postBoard(PostBoardRequestDto dto, String email) {
@@ -58,5 +85,4 @@ public class BoardServiceImplement implements BoardService {
 
         return PostBoardResponseDto.success();
     }
-
 }
